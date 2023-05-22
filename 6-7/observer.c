f91bc845b3703e29d8409063cb0a20fb6f9ecdd0
aliyagabdullina <95296268+aliyagabdullina@users.noreply.github.com>
2023-05-22 22:45:42 +0300

    Create observer.c

---
+++ 6-7/observer.c
@@ -0,0 +1,53 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 8080
+
+void display_system_status(const char *status) {
+    printf("Observer: %s\n", status);
+}
+
+int main() {
+    int observer_socket;
+    struct sockaddr_in server_address;
+
+    if ((observer_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, SERVER_IP, &(server_address.sin_addr)) <= 0) {
+        perror("Invalid address/ Address not supported");
+        exit(EXIT_FAILURE);
+    }
+
+    if (connect(observer_socket, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    char status_message[100];
+    while (1) {
+        memset(status_message, 0, sizeof(status_message));
+
+        // Получение информации от сервера
+        if (recv(observer_socket, status_message, sizeof(status_message), 0) <= 0) {
+            perror("Receive failed");
+            exit(EXIT_FAILURE);
+        }
+
+        display_system_status(status_message);
+    }
+
+    close(observer_socket);
+
+    return 0;
+}
